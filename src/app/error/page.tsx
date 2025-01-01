37d3ef6bbb5c240b9bf95bee843c71596620c099
Thunder <jdoxley6@gmail.com>
2025-01-01 00:47:07 -0800

    add error page + client side test

---
+++ src/app/error/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function ErrorPage() {
+    return (
+        <div>
+            <h1>Sorry, something went wrong.</h1>
+        </div>
+    )
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,24 @@
-import { signInWithDiscord } from './actions'
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+import { env } from "~/env"
+import { createClient } from "~/utils/supabase/client"
 
 export default function LoginPage() {
     return (
         <form action={signInWithDiscord}>
-            <button type='submit'>Login</button>
+            <button formAction={signInWithDiscord}>Login</button>
         </form>
     )
+}
+
+export async function signInWithDiscord() {
+    const supabase = await createClient()
+    console.log(env.NEXT_PUBLIC_URL)
+    const { data, error } = await supabase.auth.signInWithOAuth({ provider: 'discord', options: { redirectTo: `${env.NEXT_PUBLIC_URL}/auth/callback` } })
+    if (error) {
+        console.log(error)
+        redirect('/error')
+    }
+    revalidatePath('/', 'layout')
+    redirect('/')
 }
